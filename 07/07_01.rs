b6448a64bef9a8e43acab525521b2b3409b6819f
Vladimir Chernyshev <sveolon@gmail.com>
2019-12-07 09:45:42 -0800

    Update 07_01.rs
    Almost working day 7 part 1, permutations generator is buggy

--- 07/07_01.rs
+++ 07/07_01.rs
@@ -1,4 +1,4 @@
-fn int_comp(arr: [i32;17], input1: i32, input2: i32) -> i32 {
+fn int_comp(arr: [i32;34], input1: i32, input2: i32) -> i32 {
     let mut a = arr;
     let mut i = 0;
     let mut input1_used = false;
@@ -69,13 +69,63 @@ fn int_comp(arr: [i32;17], input1: i32, input2: i32) -> i32 {
     return output;
 }
 
+// Generating permutation using Heap Algorithm 
+fn heap_permutation(mut a: [usize; 5], size: usize, output: &mut Vec<[usize;5]>) 
+{ 
+    // if size becomes 1 then prints the obtained 
+    // permutation 
+    if size == 1 { 
+        output.push(a);
+        return; 
+    } 
+  
+    heap_permutation(a,size-1,output); 
+    for i in 0..size { 
+        // if size is odd, swap first and last 
+        // element 
+        if size%2==1 {
+            let tmp = a[0];
+            a[0] = a[size - 1];
+            a[size - 1] = tmp;
+        }
+  
+        // If size is even, swap ith and last 
+        // element 
+        else {
+            let tmp = a[i];
+            a[i] = a[size - 1];
+            a[size - 1] = tmp;
+        }
+        heap_permutation(a,size-1,output); 
+    } 
+} 
+
 fn main() {
-    let a = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0];
-    //let a: [i32; 11] = [3,9,8,9,10,9,4,9,99,-1,8];
-    let setting = [4,3,2,1,0];
-    let mut output = 0;
-    for i in setting.iter() {
-        output = int_comp(a, *i, output);
+    let a = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0];
+    //let a = [3,8,1001,8,10,8,105,1,0,0,21,46,59,84,93,110,191,272,353,434,99999,3,9,101,2,9,9,102,3,9,9,1001,9,5,9,102,4,9,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,102,5,9,9,4,9,99,3,9,1001,9,4,9,1002,9,2,9,101,2,9,9,102,2,9,9,1001,9,3,9,4,9,99,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,1001,9,5,9,1002,9,3,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,99,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,99];
+    let setting1 = [0,1,2,3,4];
+    let mut all_permutations = Vec::new();
+    heap_permutation(setting1, 5, &mut all_permutations);
+    let mut result = 0;
+    
+    let mut winner = setting1;
+    for setting in all_permutations {
+        let mut output = 0;
+        for i in setting.iter() {
+            output = int_comp(a, *i as i32, output);
+        }
+        
+        if output > result {
+            result = output;
+            winner = setting;
+        }
+        
+        print!("{}: ", output);
+        for i in setting.iter() {
+            print!("{},", i);
+        }
+        print!("\n");
     }
-    println!("{}", output);
+    
+    println!("{}", result);
 }
