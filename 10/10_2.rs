2513d381b04ffa15c7df98d1dac5ef36bab4fd2e
Vladimir Chernyshev <sveolon@gmail.com>
2019-12-11 08:39:15 -0800

    Update 10_2.rs
    some bugs fixed but still does not work

--- 10/10_2.rs
+++ 10/10_2.rs
@@ -1,7 +1,11 @@
 use std::collections::HashSet;
+use core::f64::consts::PI;
 
 fn main() {
-    /*let a = [
+
+    let dbg = false;
+    
+    let a = [
         ".###..#######..####..##...#",
         "########.#.###...###.#....#",
         "###..#...#######...#..####.",
@@ -29,8 +33,9 @@ fn main() {
         "#.###.#.#...##..#####.###.#",
         "#.####.#..#.#.##.######.#..",
         ".#.#####.##...#...#.##...#.",
-    ];*/
-    let a = [".#..##.###...#######","##.############..##.",".#.######.########.#",".###.#######.####.#.","#####.##.#.##.###.##","..#####..#.#########","####################","#.####....###.#.#.##","##.#################","#####.##.###..####..","..######..##.#######","####.##.####...##..#",".#####..#.######.###","##...#.##########...","#.##########.#######",".####.#.###.###.#.##","....##.##.###..#####",".#.#.###########.###","#.#.#.#####.####.###","###.##.####.##.#..##"];
+    ];
+    //let a = [".#....#####...#..","##...##.#####..##","##...#...#.#####.","..#.....#...###..","..#.#.....#....##"];
+    //let a = [".#..#",".....","#####","....#","...##"];
     let y_l = a.len();
     let x_l = a[0].len();
 
@@ -71,16 +76,31 @@ fn main() {
         }
         
         let x = other.0 - center.0;
-        let y = other.1 - center.1;
+        let y = -(other.1 - center.1);
         
         let len_2 = (x * x + y * y) as f64;
         let mut angle = (y as f64 / x as f64).atan();
-        if y < 0 {
-            angle += std::f64::consts::PI;
+        
+        if x == 0 {
+            angle = if y > 0 { PI / 2.0 } else { 1.5 * PI };
+        } else if y == 0 {
+            angle = if x > 0 { 0.0 } else { PI };
+        } else if y < 0 && x < 0 {
+            angle += 2.0 * PI;
         } else if x < 0 {
-            angle += 2.0 * std::f64::consts::PI;
+            angle += PI;
         }
-        bearings.push((angle, len_2, other.0, other.1));
+        
+        // rotate the plain
+        angle -= PI / 2.0;
+        if angle <= 0.0 {
+            angle += 2.0 * PI;
+        }
+        
+        //invert the direction
+        angle *= -1.0;
+        
+        bearings.push((angle, len_2, other.0, other.1, false));
     }
     
     bearings.sort_unstable_by(|a, b| {
@@ -91,10 +111,38 @@ fn main() {
         }
     });
 
+    if dbg {
     for i in 0..bearings.len() {
         println!(
-            "{} {} {} {} {}",
-            i, bearings[i].0, bearings[i].1, bearings[i].2, bearings[i].3
+            "{}) a:{}\tl:{}\tx:{}\ty:{}\tused:{}",
+            i, bearings[i].0, bearings[i].1, bearings[i].2, bearings[i].3, bearings[i].4
+        );
+    } }
+    
+    let mut curr_angle = -100.0; // impossible 
+    let mut ind = 0;
+    let mut count = 0;
+    let sz = bearings.len();
+    while count < sz {
+        while num::abs(bearings[ind%sz].0 - curr_angle) < std::f64::EPSILON || bearings[ind%sz].4 {
+            ind += 1;
+        }
+        count += 1;
+        curr_angle = bearings[ind%sz].0;
+        bearings[ind%sz].4 = true;
+        
+        println!(
+            "{}] a:{} l:{} x:{} y:{} used:{}",
+            count, bearings[ind%sz].0, bearings[ind%sz].1, bearings[ind%sz].2, bearings[ind%sz].3, bearings[ind%sz].4
         );
     }
+    print!("\n\n\n");
+    
+    let i = ind%sz;
+    println!(
+            "{}) a:{} l:{} x:{} y:{} used:{}",
+            i, bearings[i].0, bearings[i].1, bearings[i].2, bearings[i].3, bearings[i].4
+        );
+        
+    println!("res: {}", bearings[i].2 * 100 + bearings[i].3);
 }
