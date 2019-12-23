185a98d32520a0146837203035b480eeab7cdf0e
Vladimir Chernyshev <sveolon@gmail.com>
2019-12-22 20:02:28 -0800

    Update 20_1.rs
    20-1 some more parsing

--- 20/20_1.rs
+++ 20/20_1.rs
@@ -51,4 +51,32 @@ FG..#########.....#
             }
         }
     }
+    let mut portals = HashMap::new();
+    let mut portals_rev = HashMap::new();
+    
+    for y in 0..max_y {
+        for x in 0..max_x {
+            if 'A' <= a[y][x] && a[y][x] <= 'Z' {
+                if x+1 < max_x && 'A' <= a[y][x+1] && a[y][x+1] <= 'Z' {
+                    let s = format!("{}{}", a[y][x], a[y][x+1]);
+                    if x+2 < max_x && a[y][x+2] == '.' {
+                        portals.insert((x+2,y), &s);
+                        portals_rev.insert(&s, (x+2,y));
+                    } else if x > 0 && a[y][x-1] == '.' {
+                        portals.insert((x-1,y), &s);
+                        portals_rev.insert(&s, (x-1,y));
+                    }
+                } else if y+1 < max_y && 'A' <= a[y+1][x] && a[y+1][x] <= 'Z' {
+                    let s = format!("{}{}", a[y][x], a[y+1][x]);
+                    if y+2 < max_y && a[y+2][x] == '.' {
+                        portals.insert((x,y+2), &s);
+                        portals_rev.insert(&s, (x,y+2));
+                    } else if y > 0 && a[y-1][x] == '.' {
+                        portals.insert((x,y-1), &s);
+                        portals_rev.insert(&s, (x,y-1));
+                    }
+                }
+            }
+        }
+    }
 }
