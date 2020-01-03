4e3a9871240d6cfa814c32002f84eec44f1123f0
Vladimir Chernyshev <sveolon@gmail.com>
2020-01-03 08:07:12 -0800

    Update 24_2.rs
    24-2

--- 24/24_2.rs
+++ 24/24_2.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let _input = "\
+    let input = "\
 #.###
 .....
 #..#.
 ##.##
 ..#.#";
 
-let input = "\
+let _input = "\
 ....#
 #..#.
 #.?##
@@ -14,9 +14,13 @@ let input = "\
 #....";
 
     let mut generation = vec![to_biodev(input)];
+    //print_field(generation[0]);
+    //print_field(gen(&generation, 1, 4, 1));
+    
+    //println!("\n\n!!!!!!!!!!!!!!!!!\n\n\n\n\n\n\n");
 
-    //for _i in 0..200 {
-    for _i in 0..1 {
+    for _i in 0..200 {
+    //for _i in 0..10 {
         let mut next_generation = Vec::new();
 
         for j in -1 as i32..generation.len() as i32 + 1 {
@@ -27,14 +31,16 @@ let input = "\
     
     let mut result = 0;
     for g in generation {
-        print_field(g);
+        if g != 0 {
+            print_field(g);
+        }
         for i in 0..32 {
             if g & 1 << i != 0 {
                 result += 1;
             }
         }
     }
-    println!("result: {}", result);
+    println!("\nresult: {}", result);
 }
 
 fn next_gen(current: &Vec<i32>, i: i32) -> i32 {
@@ -57,26 +63,32 @@ fn gen(current: &Vec<i32>, layer: i32, x: i32, y: i32) -> i32 {
 
         let mut i = x + f.0;
         let mut j = y + f.1;
-
-        let l2 = layer as usize + 1;
+        
+        let l2 = (layer + 1) as usize;
+        
+        //println!("gen l {}; i {}; j {}; l2 {}; current.len() {}", l, i, j, l2, current.len());
+                
         if i == 2 && j == 2 && current.len() > l2 {
             // count inside neighbours
             for k in 0..5 {
                 let index;
-                if x == 2 {
+                if x == 1 {
                     index = k * 5 + 0;
-                } else if x == 4 {
+                } else if x == 3 {
                     index = k * 5 + 4;
-                } else if y == 2 {
+                } else if y == 1 {
                     index = 0 * 5 + k;
-                } else /*if y == 4 */ {
+                } else /*if y == 3 */ {
                     index = 4 * 5 + k;
                 }
+                //println!("k {}; index {}; (current[l2] & 1 << index) {}", k, index, (current[l2] & 1 << index));
                 if (current[l2] & 1 << index) != 0 {
                     n += 1;
                 }
             }
         } else {
+            //println!(">>gen layer {}; l {}; x {}; y {}; i {}; j {}", layer, l, x, y, i, j);
+
             if i < 0 || i >= 5 || j < 0 || j >= 5 {
                 l -= 1;
             }
@@ -86,17 +98,21 @@ fn gen(current: &Vec<i32>, layer: i32, x: i32, y: i32) -> i32 {
             }
             if i < 0 {
                 i = 1;
+                j = 2;
             } else if i >= 5 {
                 i = 3;
+                j = 2;
             }
             if j < 0 {
                 j = 1;
+                i = 2
             } else if j >= 5 {
                 j = 3;
+                i = 2;
             }
 
             let index = j * 5 + i;
-            //println!("gen {} {} {}; {} {}", layer, x, y, i, j);
+            //println!("<<gen layer {}; l {}; x {}; y {}; i {}; j {}\n", layer, l, x, y, i, j);
             if (current[l as usize] & 1 << index) != 0 {
                 n += 1;
             }
@@ -110,6 +126,8 @@ fn gen(current: &Vec<i32>, layer: i32, x: i32, y: i32) -> i32 {
     } else {
         is_occupied = current[layer as usize] & (1 << cur_index) != 0;
     }
+    
+    //println!("\ngen({:?},{},{},{}); n == {}, is_occ = {}", current, layer, x, y, n, is_occupied);
 
     if is_occupied {
         return if n == 1 { 1 << cur_index } else { 0 };
@@ -143,4 +161,5 @@ fn print_field(f: i32) {
             print!("{}", if is_occupied { "# " } else { ". " });
         }
     }
+    print!("\n");
 }
